957ee74f202cae4410829a5bf3728f826e412729
naruto2922004 <anishagarwal2922004@gmail.com>
2026-07-05 21:37:03 +0530

    Initial USB implementation

--- shared/crc16_engine.v
+++ shared/crc16_engine.v
@@ -1,7 +1,6 @@
 // CRC engine runs continuously.
 // Assert reset before processing each new packet.
 // CRC output is valid only immediately after the last valid input bit.
-`include "../usb_pkg.vh"
 
 module crc16_engine (
     input rst_n, clk, data_in,
@@ -11,10 +10,10 @@ module crc16_engine (
 reg [15:0] crc16;
 always @(posedge clk or negedge rst_n) begin
     if (!rst_n)
-        crc16 <= USB_CRC16_INIT;
+        crc16 <= 16'hFFFF;
     else begin
         if(data_in^crc16[15])
-            crc16 <= {crc16[14:0], 1'b0} ^ USB_CRC16_POLY;
+            crc16 <= {crc16[14:0], 1'b0} ^ 16'h8005;
         else
             crc16 <= {crc16[14:0], 1'b0};
     end
--- shared/crc5_engine.v
+++ shared/crc5_engine.v
@@ -1,7 +1,6 @@
 // CRC engine runs continuously.
 // Assert reset before processing each new packet.
 // CRC output is valid only immediately after the last valid input bit.
-`include "../usb_pkg.vh"
 module crc5_engine (
     input rst_n, clk, data_in,
     output reg [4:0]rx
@@ -10,10 +9,10 @@ module crc5_engine (
 reg [4:0] crc5;
 always @(posedge clk or negedge rst_n) begin
     if (!rst_n)
-        crc5 <= USB_CRC5_INIT;
+        crc5 <= 5'b11111;
     else begin
         if(data_in^crc5[4])
-            crc5 <= {crc5[3:0], 1'b0} ^ USB_CRC5_POLY;
+            crc5 <= {crc5[3:0], 1'b0} ^ 5'b00101;
         else
             crc5 <= {crc5[3:0], 1'b0};
     end
